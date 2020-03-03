d23c420fb3e2769d747bbf1cc1215bfe00e2d1a9
Tanvir Rahman <tanvir.rahman.ewu@gmail.com>
2020-03-03 15:27:38 +0600

    new component added called hello world(just for test)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Components/helloworld'
+import helloworld from './Components/helloworld';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h3>hello world</h3>
+        <helloworld/>
       </header>
     </div>
   );
